ac5d6e73aaa63c5cc0069d5500cdc880bd22f040
Peter Bengtsson <mail@peterbe.com>
2022-01-07 22:32:11 -0500

    End to end tests and coping with pseudo selectors (#12)
    * end-to-end tests and coping with pseudo selectors
    
    * delete some

---
+++ test/end-to-ends.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+
+import { minimize } from "../src";
+
+describe("End-to-ends", () => {
+  const rt = "test/fixtures";
+  const fixtures = fs.readdirSync(rt).map((name) => path.join(rt, name));
+
+  for (const fixture of fixtures) {
+    it(`should cope with <${path.basename(fixture)}>`, () => {
+      const html = fs.readFileSync(path.join(fixture, "html.html"), "utf-8");
+      const css = fs.readFileSync(path.join(fixture, "css.css"), "utf-8");
+
+      const { finalCSS, sizeBefore, sizeAfter } = minimize({ html, css });
+      expect(sizeAfter < sizeBefore).toBeTruthy();
+      expect(finalCSS).toBeTruthy();
+    });
+  }
+});
